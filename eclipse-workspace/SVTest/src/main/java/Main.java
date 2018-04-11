7141296f10fe8eaf26c7ff0d8fa6342ef7e0d4d9
Jongwoo <Jongwoo@DESKTOP-0N4LL8P>
2018-04-11 13:41:35 +0900

    끄으으으

--- eclipse-workspace/SVTest/src/main/java/Main.java
+++ eclipse-workspace/SVTest/src/main/java/Main.java
@@ -3,7 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Hello java!");
+		System.out.println("Hello world");
+
 	}
 
 }
